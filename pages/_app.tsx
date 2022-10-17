0eaa0e13dae157171651bb1a1c39f8e17823f919
stracciatella27 <schoko.bombe44@gmail.com>
2022-10-17 20:32:13 +0200

    add hero

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+// @ts-ignore
+import AOS from "aos";
+
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 50,
+    });
+  }, []);
   return <>
     <Head>
         <title>Michael Pfister</title>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,36 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { motion } from "framer-motion"
+
 
 const Hero = () => {
-  return
+  return <section className='flex flex-col md:items-center my-16 bg-[url("/low-poly-grid.svg")] bg-cover bg-no-repeat md:flex-row items-end'>
+    <motion.div className='w-full flex justify-center p-4' initial={{ scale: 0, rotate: 180 }}
+      animate={{ rotate: 360, scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20
+    }}>
+      <h1>
+          <text className='text-5xl md:text-7xl'>Hi, I{"'"}m Michael! 👋</text>
+          <br/>
+          <br/>
+          <text className='text-3xl md:text-5xl'>
+            A Professional Web Developer
+          </text>
+      </h1>
+    </motion.div>
+    <div className='w-[400px] md:w-[800px] aspect-[2692/2284] relative'>
+      <Image src="/hero.webp" alt='Michael Pfister working on a laptop' layout='fill' />
+    </div>
+  </section>
 }
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className="text-9xl">
-        Hello world!
-      </h1>
+      <Hero />
     </>
   )
 }
