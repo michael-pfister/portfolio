568e22a5ba2a7fe188843d7d4c417d39cfe98f98
stracciatella27 <schoko.bombe44@gmail.com>
2022-10-26 23:01:02 +0200

    update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    supportFile: false, // disable default support file (because I dont have one)
   },
-  chromeWebSecurity: false // this is needed to allow cypress to access the formsubmit redirect
+  chromeWebSecurity: false, // this is needed to allow cypress to access the formsubmit redirect
 });
